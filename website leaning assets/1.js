078d4c0927d6055fe5959dddecc706c095c8834d
thyuiliu <119911389+thyuiliu@users.noreply.github.com>
2022-12-14 14:35:32 +0800

    Add files via upload

---
+++ website leaning assets/1.js
@@ -0,0 +1,19 @@
+var a = 1;
+b = 35;
+c = 64;
+var user = {
+    username: "Tyhui",
+    Permit:"phiFJSD1435",
+    password: "tyhyui14"
+};
+//ºÏ—È√‹‘ø
+function CheckPermit(Input) {
+    var Permit = {
+        Permit1 = "phi114514",
+        Permit2 = "phi1919810",
+        Permit3 = "phiFJSD1435"
+    }
+    document.getElementById("check").innerHTML = "Pass!Welcome!";
+
+}
+
